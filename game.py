2fdd2dd1035ebdacc70ecf1fff17f4092efb936c
Evee Carter <public@genevievecarter.com>
2018-03-08 16:04:01 -0700

    Basic target.

--- game.py
+++ game.py
@@ -4,20 +4,21 @@ import Adafruit_GPIO.I2C as I2C
 import Adafruit_PCA9685
 import Mux
 import Adafruit_TCS34725
+import RPi.GPIO as GPIO
 
 i2c = I2C
 servo = Adafruit_PCA9685.PCA9685(address=0x40, busnum=1)
-servo_min = 150  # Min pulse length out of 4096
+servo_min = 180 # 150  # Min pulse length out of 4096
 servo_max = 600  # Max pulse length out of 4096
-servo_mid = 400
+servo_mid = 350
 mux = Mux.Mux()
-
+GPIO.setmode(GPIO.BOARD)
 # Set frequency to 60hz, good for servos.
 servo.set_pwm_freq(60)
 
 
 class Target(object):
-    def __init__(self, mux, muxnum, servo, servonum):
+    def __init__(self, mux, muxnum, servo, servonum, lednum):
         self.mux = mux
         self.muxnum = muxnum
         self.servo = servo
@@ -26,24 +27,41 @@ class Target(object):
         self.mux.channel(self.muxnum)
         self.sensor = Adafruit_TCS34725.TCS34725(address=0x29, busnum=1)
 
+        self.lednum = lednum
+        GPIO.setup(lednum, GPIO.OUT)
+
+    def led_on(self):
+        GPIO.output(self.lednum, True)
+        
+    def led_off(self):
+        GPIO.output(self.lednum, False)
+        
     def reading(self):
         self.mux.channel(self.muxnum)
         r, g, b, c = self.sensor.get_raw_data()
         return r, g, b, c
-        
-        
-servo.set_pwm(0, 0, servo_mid)
-t1 = Target(mux, 0, servo, 1)
-t2 = Target(mux, 1, servo, 2)
-t3 = Target(mux, 2, servo, 3)
 
+    def up(self):
+        self.servo.set_pwm(0, self.servonum, servo_mid)
+        self.led_on()
+
+    def down(self):
+        self.servo.set_pwm(0, self.servonum, servo_min)
+        self.led_off()
+        
+t1 = Target(mux, 0, servo, 1, 7)
+t2 = Target(mux, 1, servo, 2, 11)
+
+t1.up()
 
 while True:
+    time.sleep(0.1)
     r1, g1, b1, c1 = t1.reading()
-    r2, g2, b2, c2 = t2.reading()
-    r3, g3, b3, c3 = t3.reading()
-    print("1: {} {} {} {}    2: {} {} {} {}    3: {} {} {} {}".format(r1, g1, b1, c1, r2, g2, b2, c2, r3, g3, b3, c3))
-    time.sleep(1)
+    print(b1)
+    if b1 >= 1000:
+        t1.down()
+        time.sleep(3)
+        t1.up()
 
 # servo.set_pwm(0, 0, servo_max)
 
